c14b0c332504fd581224ec0c5d8fa96487895d6d
Vorobyev Ivan <ivorob@gmail.com>
2019-11-13 21:38:37 +0300

    Add socket test

---
+++ tests/SocketTest.cpp
@@ -0,0 +1,34 @@
+#include <gtest/gtest.h>
+
+#ifdef __APPLE_CC__
+#include <sys/socket.h>
+#include <arpa/inet.h>
+#endif 
+
+#include "ssdp/UDPSocket.h"
+
+TEST(SocketTest, createDestroyTest)
+{
+    int sockfd = -1;
+
+    struct sockaddr_in destAddr = {0};
+    destAddr.sin_family = AF_INET;
+    destAddr.sin_port = htons(8000);
+    destAddr.sin_addr.s_addr = inet_addr("239.255.255.250");
+
+    {
+        ssdp::UDPSocket socket;
+        sockfd = socket.getNative();
+        int result = sendto(sockfd, reinterpret_cast<const void *>("test"), 4, 0,
+                reinterpret_cast<const struct sockaddr *>(&destAddr), sizeof(destAddr));
+        ASSERT_TRUE(result > 0);
+
+        result = sendto(sockfd, reinterpret_cast<const void *>("test"), 4, 0,
+                reinterpret_cast<const struct sockaddr *>(&destAddr), sizeof(destAddr));
+        ASSERT_TRUE(result > 0);
+    }
+
+    int result = sendto(sockfd, reinterpret_cast<const void *>("test"), 4, 0,
+            reinterpret_cast<const struct sockaddr *>(&destAddr), sizeof(destAddr));
+    ASSERT_FALSE(result > 0);
+}
